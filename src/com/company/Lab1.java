f49ad19474062de517841f289a15222fa284fb0b
Kulitskov <kulitskovs@yandex.ru>
2021-02-15 11:54:35 +0300

    Com3

--- src/com/company/Lab1.java
+++ src/com/company/Lab1.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Lab1 {
     public static void main(String[] args) {
+        args = new String[]{"1","2","3","4","5","6","7","8","9"};
         Point3d t1 = new Point3d();
         Point3d t2 = new Point3d();
         Point3d t3 = new Point3d();
@@ -15,6 +16,7 @@ public class Lab1 {
             t3.setX(Double.parseDouble(args[6]));
             t3.setY(Double.parseDouble(args[7]));
             t3.setZ(Double.parseDouble(args[8]));
+            System.out.println(t1.getX());
             System.out.println(computeArea(t1, t2, t3));
         }
     }
